9616520795dcc7c4f861a32c8d70bff6ba97819a
shevalekaran <37209238+shevalekaran@users.noreply.github.com>
2024-01-28 23:01:20 +0530

    first commit

---
+++ src/survey/management/system/SurveyManagementSystem.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package survey.management.system;
+
+import java.sql.SQLException;
+
+/**
+ *
+ * @author hp
+ */
+public class SurveyManagementSystem {
+
+    
+	public static void main(String args[]) throws SQLException {
+		Login login = new Login();
+		login.loginView();
+	}
+    }
+
+    
